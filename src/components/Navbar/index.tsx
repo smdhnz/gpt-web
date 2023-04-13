f7700e1498ade86d004620fb1ecac5c1f05771d1
smdhnz <strikerfmy1208@gmail.com>
2023-04-13 09:57:18 +0900

    ✨Add chat clear button

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,14 +1,23 @@
-import { Navbar as MtNavbar, Stack, Divider } from "@mantine/core";
+import { Navbar as MtNavbar, Stack, Divider, Button } from "@mantine/core";
 import { ColorSchemeToggle } from "../ColorSchemeToggle";
 import { APIKeyInput } from "../APIKeyInput";
+import { useSetAtom } from "jotai";
+import { messagesAtom } from "../../atoms/messageAtom";
 
 export const Navbar = () => {
+  const setMessages = useSetAtom(messagesAtom);
+  const clear = () => setMessages([]);
+
   return (
     <MtNavbar width={{ xs: 250 }}>
       <Stack p="1rem">
         <ColorSchemeToggle />
         <Divider />
         <APIKeyInput />
+        <Divider />
+        <Button color="dark" variant="default" onClick={clear}>
+          Clear
+        </Button>
         <Divider />
       </Stack>
     </MtNavbar>
